6c3ffc27ee000eedc2d3feb6a8fa519498e2caef
Oleh Bovsunivskyi <olehb@earnix.com>
2017-01-05 19:06:11 +0200

    tests added

--- src/Tests.java
+++ src/Tests.java
@@ -1,10 +1,51 @@
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tests
 {
     public static void main(String[] args)
     {
-        typeCasting();
+        testFinallySout();
+    }
+    
+    static void testFinallySout() {
+        System.out.println(testFinally());
+    }
+
+    //fucking finally block
+    static int testFinally() {
+        try{
+            if(1==1) throw new Error();
+        }catch(Exception e){
+            return 0;
+        }
+        finally
+        {
+            return 1;
+        }
+    }
+    
+    static void testNull() {
+        String s = null;
+        String s2 = s+"a";
+        System.out.println(s2);
+    }
+    
+    static class B {}
+    static class A {}
+    static class AA extends A {}
+    
+    public static void genericExtends() {
+        B b = new B();
+        A a = new A();
+        AA aa = new AA();
+        List<? extends A> list = new ArrayList<>();
+        method(a.getClass());
+//        list.add(new Object());
+    }
+    
+    public static void method(Class<? extends A> clas) {
     }
 
     private static void typeCasting()

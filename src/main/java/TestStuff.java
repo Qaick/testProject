377da4f742248c9055131c6fe1e02810897f7c3f
Oleh Bovsunivskyi <olehb@earnix.com>
2017-06-20 12:24:11 +0300

    a

--- src/main/java/TestStuff.java
+++ src/main/java/TestStuff.java
@@ -5,8 +5,22 @@ public class TestStuff {
     public static void main(String[] args)
     {
         List<String> list = new ArrayList<>();
+        list.add("before first");
         list.add("first");
         
         System.out.println(String.format("%10s |%5s| end", "first", list.get(1)));
+    }
+    static class FooObject {
+        public static void saySomething() {
+            System.out.println("Hi there!");
+        }
+    }
+
+    static class Main {
+        public static void main(String[] args) {
+            int[] foo = null;
+            System.out.println(foo.length);
+            
+        }
     }
 }

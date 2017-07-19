e16017e92e1be9cde924dd65b3a0607127d997b7
Oleh Bovsunivskyi <olehb@earnix.com>
2017-07-19 12:18:09 +0300

    jar

--- src/main/java/jit/generics/ExplicitTypeSpecification.java
+++ src/main/java/jit/generics/ExplicitTypeSpecification.java
@@ -1,11 +1,11 @@
 package jit.generics;//: generics/ExplicitTypeSpecification.java
-//import typeinfo.pets.*;
-import java.util.*;
-import net.mindview.util.*;
+
+import java.util.List;
+import java.util.Map;
 
 public class ExplicitTypeSpecification {
   static void f(Map<Person, List<Pet>> petPeople) {}
   public static void main(String[] args) {
-    f(New.<Person, List<Pet>>map());
+    f(New.map());
   }
 } ///:~

18a75631ceaa68f42fda63f20894919b1c5fd8a0
Oleh Bovsunivskyi <qaick@rambler.ru>
2017-09-09 12:46:21 +0300

    codility

--- src/main/java/codility/BinaryPalindrome.java
+++ src/main/java/codility/BinaryPalindrome.java
@@ -1,38 +1,60 @@
 package codility;
 
-import org.apache.commons.lang3.StringUtils;
-
 public class BinaryPalindrome {
     public static void main(String[] args) {
-        // 1 11, 101 111 1001 1111, 10001 10101 11011 11111 100001 101101 110011
-        int n = 20;
+        int n = 100;
         String[] arr = new String[n];
         arr[0] = "1";
         arr[1] = "11";
-        arr[2] = "101";
-        arr[3] = "111";
-        int exp = 1;
+        int exp = 0;
         int idx = 0;
-        boolean secondTime = false;
-        for (int i = 4; i < n; i++) {
-            String number = String.format("%" + String.valueOf(exp ) + "s", Integer.toString(idx++,2)).replace(' ','0');
+        boolean secondTime = true;
+        for (int i = 2; i < n; i++) {
+            String number = String.format("1%" + String.valueOf(exp+1) + "s", Integer.toString(idx,2)).replace(' ','0');
+            idx++;
+            String reverse = new StringBuilder(number).reverse().toString();
             if (secondTime) { // odd
-                String reverse = StringUtils.reverse(number);
-                number = number.substring(0, number.length() - 2) + reverse;
+                number = number.substring(0, number.length() - 1) + reverse;
             } else { // even
-                number = number.concat(number);
+                number += reverse;
             }
-            arr[i] = "1" + number + '1';
-            if (idx >= (2 << exp)-1) {
+            arr[i] = number;
+            if (idx >= (2 << exp)) {
                 idx = 0;
                 if (secondTime) {
-                    exp++;
                     secondTime = false;
                 } else {
+                    exp++;
                     secondTime = true;
                 }
             }
-            System.out.println(arr[i]);
+            System.out.println(arr[i]+" "+Integer.valueOf(arr[i],2));
         }
     }
 }
+/*
+1
+11
+101
+111
+1001
+1111
+10001
+10101
+11010
+11111
+100001
+101101
+110011
+111111
+1000001
+1001001
+1010101
+1011101
+1100011
+1101011
+1110111
+1111111
+10000001
+10011001
+ */

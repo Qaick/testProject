c5582d15ebf33da6b2167a2457e81331ecc89ea8
Oleh Bovsunivskyi <olehb@earnix.com>
2017-09-28 15:49:27 +0300

    switch

--- src/main/java/AmazingSwitch.java
+++ src/main/java/AmazingSwitch.java
@@ -1,10 +1,18 @@
 import java.nio.charset.Charset;
 
-public class Delete {
+public class AmazingSwitch {
 
     public static void main(String[] args)
     {
         System.out.println(Charset.forName("utf-8"));
         System.out.println(Charset.forName("UTF-8"));
+        
+        // Amazing switch
+        switch (1) {
+            case 0:
+                int i=1;
+            case 1:
+                i=5;
+        }
     }
 }

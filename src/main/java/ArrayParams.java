945e758dd87f500afb2043b9889203b20b698932
Oleh Bovsunivskyi <olehb@earnix.com>
2017-09-28 10:04:00 +0300

    else

--- src/main/java/ArrayParams.java
+++ src/main/java/ArrayParams.java
@@ -1,15 +1,29 @@
 import java.util.Arrays;
 
 public class ArrayParams {
-    public static void main(String[] args) {
-        meth(null);
-        meth(null, null);
-        meth(null, "string");
-        meth("string", null);
-        meth(null, null, null, "asdf");
+    public class Test
+    {
+        String a = "";
+        public void method()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                System.out.print(i);
+            }
+            System.out.print("");
+        }
     }
     
-    static void meth(String... arr) {
+    public static void main(String[] args) {
+//        meth(null);
+//        meth(null, null);
+//        meth(null, "string");
+//        meth("string", null);
+//        meth(null, null, null, "asdf");
+        meth(2, 1, "");
+    }
+
+    static void meth(int a, int b, String... arr) {
         System.out.println("arr = " + Arrays.toString(arr));
     }
 }
---
+++ src/main/java/java8/UseAnnotation.java
@@ -0,0 +1,7 @@
+package java8;
+
+@MyAnnotation
+public class UseAnnotation {
+    public static void main(String[] args) {
+    }
+}

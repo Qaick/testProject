9e4e4bc050013cff509ef70255ab3a8a46893b3e
Oleh Bovsunivskyi <oleh.bovsunivskyi@earnix.com>
2017-10-09 15:06:46 +0300

    junit4 tests with rules

---
+++ src/main/java/ForTests.java
@@ -0,0 +1,6 @@
+public class ForTests {
+    public static void method() {
+        System.out.println("hello we have problem here");
+        if (true) throw new RuntimeException("some Message 1");
+    }
+}
---
+++ src/test/java/ForTestsTest.java
@@ -0,0 +1,23 @@
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.CoreMatchers.startsWith;
+
+public class ForTestsTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    
+    @Test
+    public void method() throws Exception {
+        thrown.expectMessage(startsWith("some Message"));
+        ForTests.method();
+    }
+    
+    @Test
+    public void method2() throws Exception {
+        thrown.expectMessage(startsWith("wrong some Message"));
+        ForTests.method();
+    }
+
+}

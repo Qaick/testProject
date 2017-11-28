66403b5c66006044975bcb1a42cc87630f01cdcc
Oleh Bovsunivskyi <oleh.bovsunivskyi@earnix.com>
2017-11-28 14:31:43 +0200

    Did a good job testing inner class privacy.

--- module1/src/HelloW.java
+++ module1/src/HelloW.java
@@ -2,7 +2,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class HelloW {
+    
+    private static class Inner{
+        private int someValue = 1;
+        int getSomeValue() {return someValue;}
+    }
     public static void main(String[] args) {
+        Inner hi = new Inner();
+        hi.someValue = 4;
+        System.out.println("someValue = " + hi.getSomeValue());
+        if (++hi.someValue % 5 == 0) {
+            System.out.println("OK");
+        }
+        
         System.out.println("hello");
         List<String> list = Arrays.asList("hey", "no", "tototo");
         final String[] a = new String[list.size()];
